67ff2bc1a400f950fc714a1542d1f272d802f589
alexissavin <alexis.savin@gmail.com>
2022-09-21 16:10:03 +0200

    Update examples

--- solidserver/data_source_ip_space.go
+++ solidserver/data_source_ip_space.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
