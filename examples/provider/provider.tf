a8b26288e24258d654d225ea03a7a9ae951ec8b1
alexissavin <alexis.savin@gmail.com>
2022-09-21 14:44:04 +0200

    Cleanup

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,7 +1,6 @@
-# Configure the Cloudflare provider using the required_providers stanza
+# Configure the SOLIDserver provider using the required_providers stanza
 # required with Terraform 0.13 and beyond. You may optionally use version
 # directive to prevent breaking changes occurring unannounced.
-
 
 terraform {
   required_providers {
